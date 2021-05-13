7e8472cc0663d0e44931f79166fd0038c903f97d
jiminy-kirket <61626164+jiminy-kirket@users.noreply.github.com>
2021-05-13 09:42:18 -0400

    Added init to AboutUsViewController

--- QuizApp/UI/Login/AboutUsViewController.swift
+++ QuizApp/UI/Login/AboutUsViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 class AboutUsViewController: BaseViewController {
 
     @IBOutlet weak var shibaGIF: UIImageView!
+   
+    let remoteAPI: RemoteAPI
+    
+    init(remoteAPI: RemoteAPI) {
+        self.remoteAPI = remoteAPI
+        super.init(nibName: "AboutUsViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
